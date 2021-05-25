4d38e9ac1257940ccd1efd06b1ee7e437ee1f500
madinur mahmud <madinur1206@gmail.com>
2021-05-25 19:24:26 -0400

    shared on git hub

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,10 @@
 public class BranchingPractice {
     public static void main(String[] args) {
+        //created new project
+        //enabled version control
+        //added .gitignore with.idea target folder and .imlfile
+        //made  initial commit by keepimg track of all files
+        //shared project on github
 
     }
 }
