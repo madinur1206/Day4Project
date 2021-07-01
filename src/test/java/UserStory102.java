1fa97a3c1b2e44668661fb20a0b060c6579b8b6b
madinur1206 <82923435+madinur1206@users.noreply.github.com>
2021-06-30 21:28:12 -0400

    Us102  has been completed (#4)
    * user story 102 has been created.
    
    * user story 102 has been committed.  
    
    * user story 102 has been changed

---
+++ src/test/java/UserStory102.java
@@ -0,0 +1,8 @@
+public class UserStory102 {
+    //a lot of code
+    //readu to push this branch to remote
+    //....
+    //I will push so my team memeber can review an approve
+    //I made some changes according to request
+
+}
